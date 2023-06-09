d6d9dd08192416137dcdace473bbbe323fe8cd47
maiyq1 <U202010305@upc.edu.pe>
2023-06-08 21:55:09 -0500

    Inicio del proyecto

---
+++ src/main/java/pe/edu/upc/schoolblog/SchoolblogApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.schoolblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolblogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolblogApplication.class, args);
+  }
+}
---
+++ src/test/java/pe/edu/upc/schoolblog/SchoolblogApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.schoolblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
