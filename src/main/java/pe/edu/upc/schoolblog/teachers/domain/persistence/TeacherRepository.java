4c2927233a950e24be4b8395e68a39fb4582c6cb
Mijael Yen Quispe <mijael.yq2002@gmail.com>
2023-06-11 16:52:28 -0500

    hotfix: teacher repo y mapper

--- src/main/java/pe/edu/upc/schoolblog/teachers/domain/persistence/TeacherRepository.java
+++ src/main/java/pe/edu/upc/schoolblog/teachers/domain/persistence/TeacherRepository.java
@@ -1,1 +1,8 @@
+package pe.edu.upc.schoolblog.teachers.domain.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.schoolblog.teachers.domain.model.entity.Teacher;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+}
